5265338fa7c7f675a5368590c7a39025fabc9396
ivanbokii <bokiyis@gmail.com>
2014-06-19 11:06:18 +0100

    output csv

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 [![David Dependency Overview](https://david-dm.org/B2MSolutions/elemez2csv.png "David Dependency Overview")](https://david-dm.org/B2MSolutions/elemez2csv)
 
 Use the elemez raw data endpoint to extract data to CSV.
+
+## Contributors
+Pair programmed by [Roy Lines](http://roylines.co.uk) and [Ivan Bokii](https://github.com/ivanbokii).
--- test/elemez2csv.js
+++ test/elemez2csv.js
@@ -1,4 +1,5 @@
-var expect = require('chai').expect,
+var _ = require('lodash'),
+  expect = require('chai').expect,
   request = require('request'),
   sinon = require('sinon'),
   elemez2csv = require('../lib/elemez2csv');
@@ -19,20 +20,40 @@ describe('elemez2csv', function() {
 
   describe('with one page of data', function() {
     beforeEach(function() {
+      sinon.stub(console, 'log');
       sinon.stub(request, 'get');
+      var events = _.map(_.range(0, 5), function(i) {
+        return {
+          key: 'k' + i,
+          scheme: 'sch' + i,
+          schemeid: 'sid' + i,
+          received: i,
+          raised: i,
+          sender: 'sdr' + i,
+          source: 'src' + i,
+          type: 't' + i,
+          data: {
+            a: i
+          }
+        };
+      });
+
       var response0 = {
         lastKey: 'X',
-        events: []
+        events: events
       };
       var response1 = {
         lastKey: null,
-        events: []
+        events: [
+
+        ]
       };
       request.get.onCall(0).yields(null, null, response0);
       request.get.onCall(1).yields(null, null, response1);
     });
 
     afterEach(function() {
+      console.log.restore();
       request.get.restore();
     });
 
@@ -58,6 +79,17 @@ describe('elemez2csv', function() {
           }
         };
         expect(request.get.args[1][0]).to.deep.equal(options1);
+        return done();
+      });
+    });
+    it('should console.log the data as CSV', function(done) {
+      return elemez2csv(argv, function() {
+        expect(console.log.callCount).to.equal(5);
+        expect(console.log.args[0][0]).to.equal('k0,sch0,sid0,0,0,sdr0,src0,t0');
+        expect(console.log.args[1][0]).to.equal('k1,sch1,sid1,1,1,sdr1,src1,t1');
+        expect(console.log.args[2][0]).to.equal('k2,sch2,sid2,2,2,sdr2,src2,t2');
+        expect(console.log.args[3][0]).to.equal('k3,sch3,sid3,3,3,sdr3,src3,t3');
+        expect(console.log.args[4][0]).to.equal('k4,sch4,sid4,4,4,sdr4,src4,t4');
         return done();
       });
     });
