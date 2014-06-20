190c6a58854aa3e46effb585b7cf399778e1d922
roylines <roy@roylines.co.uk>
2014-06-20 10:10:19 +0100

    output ISO dates instead of epoch ticks

--- test/elemez2csv.js
+++ test/elemez2csv.js
@@ -27,8 +27,8 @@ describe('elemez2csv', function() {
           key: 'k' + i,
           scheme: 'sch' + i,
           schemeid: 'sid' + i,
-          received: i,
-          raised: i,
+          received: Date.UTC(2014, i, 1),
+          raised: Date.UTC(2014, i, 2),
           sender: 'sdr' + i,
           source: 'src' + i,
           type: 't' + i,
@@ -85,11 +85,11 @@ describe('elemez2csv', function() {
     it('should console.log the data as CSV', function(done) {
       return elemez2csv(argv, function() {
         expect(console.log.callCount).to.equal(5);
-        expect(console.log.args[0][0]).to.equal('k0,sch0,sid0,0,0,sdr0,src0,t0');
-        expect(console.log.args[1][0]).to.equal('k1,sch1,sid1,1,1,sdr1,src1,t1');
-        expect(console.log.args[2][0]).to.equal('k2,sch2,sid2,2,2,sdr2,src2,t2');
-        expect(console.log.args[3][0]).to.equal('k3,sch3,sid3,3,3,sdr3,src3,t3');
-        expect(console.log.args[4][0]).to.equal('k4,sch4,sid4,4,4,sdr4,src4,t4');
+        expect(console.log.args[0][0]).to.equal('k0,sch0,sid0,2014-01-01T00:00:00.000Z,2014-01-02T00:00:00.000Z,sdr0,src0,t0');
+        expect(console.log.args[1][0]).to.equal('k1,sch1,sid1,2014-02-01T00:00:00.000Z,2014-02-02T00:00:00.000Z,sdr1,src1,t1');
+        expect(console.log.args[2][0]).to.equal('k2,sch2,sid2,2014-03-01T00:00:00.000Z,2014-03-02T00:00:00.000Z,sdr2,src2,t2');
+        expect(console.log.args[3][0]).to.equal('k3,sch3,sid3,2014-04-01T00:00:00.000Z,2014-04-02T00:00:00.000Z,sdr3,src3,t3');
+        expect(console.log.args[4][0]).to.equal('k4,sch4,sid4,2014-05-01T00:00:00.000Z,2014-05-02T00:00:00.000Z,sdr4,src4,t4');
         return done();
       });
     });
